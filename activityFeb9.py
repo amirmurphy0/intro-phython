d2234ae94a3223b0de34efe8f122432cd4cf7ba5
amirmurphy0 <158080104+amirmurphy0@users.noreply.github.com>
2024-03-19 15:42:28 +0000

    work

---
+++ activityFeb9.py
@@ -0,0 +1,10 @@
+#1. - Write a block of code where a user can enter three(3) numbers, multiply
+# those numbers together and then output the result using the int() function. 
+
+#2. - Write a block of code where a user enters a their name, age, and shoe size, 
+# the print out a sentence that says "hi my name is {user's name}. 
+# I wear size {shoeSize} and I'm {userAge} years old."
+
+#3. - what does the TypeError mean? 
+
+#4. - what does SyntaxError mean?/0
---
+++ warmupFeb14.py
@@ -0,0 +1,1 @@
+An built in function is a function that is already built. So they don't need any extra functions
